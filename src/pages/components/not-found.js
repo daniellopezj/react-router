483f336bb02b7c0ec3767deb66ebcca5fef7eae6
daniel hizo un comentario <daniellopezj0327@gmail.com>
2020-06-03 19:48:56 -0500

    add ssr to project. render from server

--- src/pages/components/not-found.js
+++ src/pages/components/not-found.js
@@ -1,15 +1,35 @@
-import React,{PureComponent} from 'react';
-import './generic-page.css'
-class PagenotFound extends PureComponent{
-    render(){
-        return(
-            <div className="Page NorFound">
-                <h1>404</h1>
-                <h1 className="SadFace">:(</h1>
-                <h2>No hemos encontrado la pagina que buscabas.</h2>
-            </div>
-        )
+import React, { PureComponent } from "react";
+import "./generic-page.css";
+class PagenotFound extends PureComponent {
+    handleBeforePage =()=>{
+        this.props.history.goBack();
+        // this.props.history.go(-2); regresa dos paginas
     }
+    handleRandomVideo =()=>{
+        const random = Math.round( Math.random()*(10-1)+1);
+        this.props.history.push(`/videos?id=${random}`);
+    }
+    handleNextPage =()=>{
+        this.props.history.goForward();
+    }
+    render() {
+    return (
+      <div className="Page NorFound">
+        <h1>404</h1>
+        <h1 className="SadFace">:(</h1>
+        <h2>No hemos encontrado la pagina que buscabas.</h2>
+        <button className="Button" onClick={this.handleBeforePage}>
+          Go to before Route
+        </button>
+        <button className="Button" onClick={this.handleRandomVideo}>
+          Random Video
+        </button>
+        <button className="Button" onClick={this.handleNextPage}>
+          Go to next Route
+        </button>
+      </div>
+    );
+  }
 }
 
 export default PagenotFound;
